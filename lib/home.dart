a0bfe066dc8ef8468dbb7518268864686b89c8fb
Kwadwo Abrokwah <abrokwakwadwo85@gmail.com>
2023-03-23 17:59:48 +0000

    added the various cards and aligned them

--- lib/home.dart
+++ lib/home.dart
@@ -13,23 +13,122 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/product.dart';
+import 'model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   // TODO: Make a collection of cards (102)
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
+    List<Card> _buildGridCards(BuildContext context) {
+      List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+      if (products.isEmpty) {
+        return const <Card>[];
+      }
+
+      final ThemeData theme = Theme.of(context);
+      final NumberFormat formatter = NumberFormat.simpleCurrency(
+          locale: Localizations.localeOf(context).toString());
+
+      return products.map((product) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          // TODO: Adjust card heights (103)
+          child: Column(
+            // TODO: Center items on the card (103)
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18 / 11,
+                child: Image.asset(
+                  product.assetName,
+                  package: product.assetPackage,
+                  // TODO: Adjust the box size (102)
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    // TODO: Align labels to the bottom and center (103)
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // TODO: Change innermost Column (103)
+                    children: <Widget>[
+                      // TODO: Handle overflowing labels (103)
+                      Text(
+                        product.name,
+                        style: theme.textTheme.headline6,
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        formatter.format(product.price),
+                        style: theme.textTheme.subtitle2,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }).toList();
+    }
+
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       // TODO: Add app bar (102)
-      appBar: AppBar(),
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        //Menu button
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu Button');
+          },
+        ),
+        title: const Text('SHRINE'), // APPBAR TITLE
+
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search, // this is for the search icon
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel:
+                  'Filter', //SemanticLabel field is a common way to add accessibility information in Flutter.
+            ),
+            onPressed: () {
+              print('Filter Button');
+            },
+          )
+        ],
       ),
+      // TODO: Add a grid view (102)
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(context) // Replace
+          ),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
     );
