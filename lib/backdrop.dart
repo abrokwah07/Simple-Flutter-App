095e5b820c8fcbee4ebc4b9def94953b4693cef8
Kwadwo Abrokwah <abrokwakwadwo85@gmail.com>
2023-03-25 01:11:45 +0000

    Update backdrop.dart

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -62,15 +62,35 @@ class _BackdropState extends State<Backdrop>
   }
 
   // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
-  Widget _buildStack() {
+  // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    const double layerTitleHeight = 48.0;
+    final Size layerSize = constraints.biggest;
+    final double layerTop = layerSize.height - layerTitleHeight;
+
+    // TODO: Create a RelativeRectTween Animation (104)
+    Animation<RelativeRect> layerAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(
+          0.0, layerTop, 0.0, layerTop - layerSize.height),
+      end: const RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    ).animate(_controller.view);
+
     return Stack(
       key: _backdropKey,
       children: <Widget>[
         // TODO: Wrap backLayer in an ExcludeSemantics widget (104)
-        widget.backLayer,
+        ExcludeSemantics(
+          child: widget.backLayer,
+          excluding: _frontLayerVisible,
+        ),
         // TODO: Add a PositionedTransition (104)
-        // TODO: Wrap front layer in _FrontLayer (104)
-        _FrontLayer(child: widget.frontLayer),
+        PositionedTransition(
+          rect: layerAnimation,
+          child: _FrontLayer(
+            // TODO: Implement onTap property on _BackdropState (104)
+            child: widget.frontLayer,
+          ),
+        ),
       ],
     );
   }
@@ -81,9 +101,12 @@ class _BackdropState extends State<Backdrop>
       elevation: 0.0,
       titleSpacing: 0.0,
       // TODO: Replace leading menu icon with IconButton (104)
+      leading: IconButton(
+        icon: const Icon(Icons.menu),
+        onPressed: _toggleBackdropLayerVisibility,
+      ),
       // TODO: Remove leading property (104)
       // TODO: Create title with _BackdropTitle parameter (104)
-      leading: Icon(Icons.menu),
       title: Text('SHRINE'),
       actions: <Widget>[
         // TODO: Add shortcut to login screen from trailing icons (104)
@@ -110,7 +133,7 @@ class _BackdropState extends State<Backdrop>
     return Scaffold(
       appBar: appBar,
       // TODO: Return a LayoutBuilder widget (104)
-      body: _buildStack(),
+      body: LayoutBuilder(builder: _buildStack),
     );
   }
 }
